8ee6a146816c75728b0cd88b039f1d83e967cc5b
William Lima <107582520+wcclima@users.noreply.github.com>
2024-05-05 23:11:42 +0200

    Upload app code

---
+++ app/enefit_prediction.py
@@ -0,0 +1,36 @@
+#import libaries
+import streamlit as st
+from streamlit_option_menu import option_menu
+import pandas as pd
+import numpy as np
+
+#give app title
+st.title('Enefit Prosumer Prediction')
+
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+
+
+# 3. CSS style definitions
+selected3 = option_menu(None, ["Home", "Consumption",  "Production", 'Weather'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
+
+
+#map
+df = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(df)
